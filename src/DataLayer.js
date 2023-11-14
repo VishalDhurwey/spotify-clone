b26b50eae80f8ff7e645c1d7a359d1030e2cab87
VishalDhurwey <vldhurwey@gmail.com>
2023-11-14 18:10:51 +0530

    commit

---
+++ src/DataLayer.js
@@ -0,0 +1,12 @@
+import { createContext, useContext, useReducer} from "react";
+
+export const DataLayerContext = createContext();
+
+export const DataLayer = ({initaialState, reducer, children}) => (
+
+    <DataLayerContext.Provider value={useReducer(reducer, initaialState)}>
+        {children}
+    </DataLayerContext.Provider>
+);
+
+export const useDataLayerValue = ()=> useContext(DataLayerContext);
